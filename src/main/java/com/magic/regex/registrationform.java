032076269525147e5739498756f5cc73afca414e
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-05-29 17:19:48 +0530

    Added_UC-2_LastName

--- src/main/java/com/magic/regex/registrationform.java
+++ src/main/java/com/magic/regex/registrationform.java
@@ -12,10 +12,24 @@ public class registrationform {
         firstName = scanner.next();
 
         if (Pattern.matches("[A-Z]{1}[a-zA-Z]{2,}$", firstName)) {
-            System.out.println("Valid First Name");
+            System.out.println("Valid");
         }
         else {
-            System.out.println("Invalid First Name");
+            System.out.println("Invalid");
+        }
+
+    }
+    public static void toCheckLastName() {
+
+        String lastName;
+        System.out.println("Enter Last Name: ");
+        lastName = scanner.next();
+
+        if (Pattern.matches("[A-Z]{1}[a-zA-Z]{2,}$", lastName)) {
+            System.out.println("Valid");
+        }
+        else {
+            System.out.println("Invalid");
         }
 
     }
@@ -23,6 +37,7 @@ public class registrationform {
     public static void main(String[] args) {
 
         toCheckFirstName();
+        toCheckLastName();
         scanner.close();
 
     }
